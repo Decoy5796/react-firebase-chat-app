f3b81851c299e92576f0b6ffe156e264925ee3ec
Decoy5796 <kkw5796@gmail.com>
2020-12-04 16:30:54 +0900

    feat: rendering chat rooms list on side panel

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 import {
   Flex,
   Modal,
@@ -20,11 +21,17 @@ import {
 import { FaRegSmileWink, FaPlus } from 'react-icons/fa';
 import firebase from '../../../firebase';
 
+const ChatRoomsList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+`;
+
 export default function ChatRooms() {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [roomName, setRoomName] = useState('');
   const [roomDesc, setRoomDesc] = useState('');
   const [roomRef, setRoomRef] = useState(firebase.database().ref('chatRooms'));
+  const [chatRooms, setChatRooms] = useState([]);
   const user = useSelector((state) => state.user.currentUser);
 
   const handleSubmit = (e) => {
@@ -60,6 +67,24 @@ export default function ChatRooms() {
     }
   };
 
+  const addChatRoomsListeners = () => {
+    const chatRoomsArray = [];
+    roomRef.on('child_added', (dataSnapshot) => {
+      chatRoomsArray.push(dataSnapshot.val());
+      setChatRooms(chatRoomsArray);
+    });
+  };
+
+  const renderChatRooms = (chatRooms) => {
+    if (chatRooms.length > 0) {
+      return chatRooms.map((room) => <li key={room.id}>#{room.name}</li>);
+    }
+  };
+
+  useEffect(() => {
+    addChatRoomsListeners();
+  }, []);
+
   return (
     <>
       <Flex align='center' justify='space-between' onClick={onOpen}>
@@ -69,6 +94,7 @@ export default function ChatRooms() {
         </Flex>
         <FaPlus />
       </Flex>
+      <ChatRoomsList>{renderChatRooms(chatRooms)}</ChatRoomsList>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
